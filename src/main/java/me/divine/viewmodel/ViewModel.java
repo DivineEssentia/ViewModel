073dd5c790352693427fc10921c241f199f9a50b
0x0D9E <edwinmcgugan@gmail.com>
2022-03-16 17:13:04 +0000

    h

---
+++ src/main/java/me/divine/viewmodel/ViewModel.java
@@ -0,0 +1,10 @@
+package me.divine.viewmodel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ViewModel implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("the ViewModel mod has been initialized");
+    }
+}
